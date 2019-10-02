6e2e8e2c76d86727774f87e0a7f4ad41f3df428e
Abdur Rehman Majeed <arm.wurfel@gmail.com>
2019-10-02 13:39:13 +0500

    Thanks Page Animation

--- src/compontents/pages/ThanksPage.js
+++ src/compontents/pages/ThanksPage.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Thanks from '../sections/thanks/Thanks';
 
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 class ThanksPage extends Component {
+    componentDidMount() {
+        AOS.init({
+            duration : 1000
+          })
+    }
     render() {
         return (
-            <div>
+            <div data-aos='fade-up'>
                 <Thanks />
             </div>
         )

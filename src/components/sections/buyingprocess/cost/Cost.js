19b40264c08ea11042a26d5d9d9aaef1834fbd14
armwurfel <arm.wurfel@gmail.com>
2019-10-09 23:41:22 +0500

    Task Changing Updates

--- src/components/sections/buyingprocess/cost/Cost.js
+++ src/components/sections/buyingprocess/cost/Cost.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Table} from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive';
 import './Cost.css';
+
+const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 601 })
+    return isDesktop ? children : null
+}
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 600 })
+    return isMobile ? children : null
+}
 
 class Cost extends Component {
     render() {
@@ -15,47 +25,83 @@ class Cost extends Component {
                     </Row>
                     <Row>
                         <Col md={8}>
-                            <Table responsive>
-                                <thead className="txt-white">
-                                    <tr>
-                                        <th className="bg-blue">
-                                            <h3 className="m-0">Innflutningsgjöld</h3>
-                                        </th>
-                                        <th className="bg-blue ">
-                                            <p className="m-0">Ávallt innifalið í tilboði</p>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="content">
-                                        <td>
-                                            <ul className="item">
-                                                <li><h4>Forskráningargjald</h4></li>
-                                                <li><h4>Þjónustugjald</h4></li>
-                                                <li><h4>Umsýslugjald</h4></li>
-                                            </ul>
-                                        </td>
-                                        <td>
-                                            <ul className="price">
-                                                <li><h4>5.200 kr</h4></li>
-                                                <li><h4>9.990 kr</h4></li>
-                                                <li><h4>239.990 kr</h4></li>
-                                            </ul>
-                                        </td>
-                                    </tr>
-                                    <tr className="total">
-                                        <td className="item"><h4>Samtals</h4></td>
-                                        <td className="price"><h4>255.180 kr</h4></td>
-                                    </tr>
-                                </tbody>
-                            </Table>
+                            <Desktop>
+                                <Table responsive>
+                                    <thead className="txt-white">
+                                        <tr>
+                                            <th className="bg-blue">
+                                                <h3 className="m-0">Innflutningsgjöld</h3>
+                                            </th>
+                                            <th className="bg-blue ">
+                                                <p className="m-0">Ávallt innifalið í tilboði</p>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="content">
+                                            <td>
+                                                <ul className="item">
+                                                    <li><h4>Forskráningargjald</h4></li>
+                                                    <li><h4>Þjónustugjald</h4></li>
+                                                    <li><h4>Umsýslugjald</h4></li>
+                                                </ul>
+                                            </td>
+                                            <td>
+                                                <ul className="price">
+                                                    <li><h4>5.200 kr</h4></li>
+                                                    <li><h4>9.990 kr</h4></li>
+                                                    <li><h4>239.990 kr</h4></li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                        <tr className="total">
+                                            <td className="item"><h4>Samtals</h4></td>
+                                            <td className="price"><h4>255.180 kr</h4></td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Desktop>
+                            <Mobile>
+                            <Table responsive className="mobile">
+                                    <thead className="txt-white">
+                                        <tr>
+                                            <th className="bg-blue">
+                                                <h3 className="m-0">Innflutningsgjöld</h3>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="content">
+                                            <td>
+                                                <ul className="item">
+                                                    <li><h4>Forskráningargjald</h4></li>
+                                                    <h4 className="txt-blue">5.200 kr</h4>
+                                                    <li><h4>Þjónustugjald</h4></li>
+                                                    <h4 className="txt-blue">9.990 kr</h4>
+                                                    <li><h4>Umsýslugjald</h4></li>
+                                                    <h4 className="txt-blue">239.990 kr</h4>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                        <tr className="total">
+                                            <td><h4 className="item">Samtals</h4><h4 className="price">255.180 kr</h4></td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Mobile>
                         </Col>
                         <Col md={4} className="p-4 other-content">
                             <h3 className="mb-4 pb-2">Afgreiðslutími</h3>
-                            <h3>Evrópa</h3>
-                            <h3 className="mb-4 pb-2">5-8 vikur</h3>
-                            <h3>Bandaríkin</h3>
-                            <h3 className="mb-4">7-12 vikur</h3>
+                            <div>
+                                <div>
+                                    <h3>Evrópa</h3>
+                                    <h3 className="mb-4 pb-2">5-8 vikur</h3>
+                                </div>
+                                <div>
+                                    <h3>Bandaríkin</h3>
+                                    <h3 className="mb-4">7-12 vikur</h3>
+                                </div>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
--- src/components/sections/header/Header.js
+++ src/components/sections/header/Header.js
@@ -6,23 +6,39 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            navExpanded: false,
+        }
+    }
+    setNavExpanded = (expanded) => {
+        this.setState({ navExpanded: expanded });
+    }
+    
+    closeNav = () => {
+        this.setState({ navExpanded: false });
+    }
     render() {
         return (
             <header>
-                <Navbar bg="white" expand="lg">
+                <Navbar bg="white" expand="lg" collapseOnSelect 
+                    onToggle={this.setNavExpanded}
+                    expanded={this.state.navExpanded}
+                >
                     <Navbar.Brand as={NavLink} to="/"><img src="/assets/images/hybrid-logo.png" alt="Hybrid Logo" /> <span> hybrid.is </span> </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mx-auto">
-                            <Nav.Link activeClassName="active" as={NavLink} to="/">Bílar á skrá</Nav.Link>
-                            <Nav.Link activeClassName="active" href="">Bílaleit</Nav.Link>
-                            <Nav.Link activeClassName="active" as={NavLink} to="/buying-process">Kaupferlið</Nav.Link>
-                            <Nav.Link activeClassName="active" as={NavLink} to="/contact">Hafa samband</Nav.Link>
+                            <Nav.Link onClick={this.closeNav} activeClassName="active" href="">Bílar á skrá</Nav.Link>
+                            <Nav.Link onClick={this.closeNav} activeClassName="active" href="">Bílaleit</Nav.Link>
+                            <Nav.Link onClick={this.closeNav} activeClassName="active" as={NavLink} to="/buying-process">Kaupferlið</Nav.Link>
+                            <Nav.Link onClick={this.closeNav} activeClassName="active" as={NavLink} to="/contact">Hafa samband</Nav.Link>
                         </Nav>
                         <Nav>
                             <div className="borderleft">
-                                <Button className="bgprimary border90" as={NavLink} to="/login" title="Nýskráning" />
-                                <Button className="bgprimary-outline border90" as={NavLink} to="/login" title="Innskráning" />
+                                <Button onClick={this.closeNav} className="bgprimary border90" as={NavLink} to="/login" title="Nýskráning" />
+                                <Button onClick={this.closeNav} className="bgprimary-outline border90" as={NavLink} to="/login" title="Innskráning" />
                             </div>
                         </Nav>
                     </Navbar.Collapse>
